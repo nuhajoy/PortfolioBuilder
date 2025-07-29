646682ea386ab11bb1a14f55cda698a573d12f21
nuhajoy <nuhaminemnebered@gmail.com>
2025-07-29 03:42:32 +0300

    updated

--- portfoliobuilder/src/pages/Welcome.jsx
+++ portfoliobuilder/src/pages/Welcome.jsx
@@ -135,7 +135,7 @@ export default function Welcome() {
         </section>
 
         {/* CTA Section */}
-        <section className="py-20 bg-gradient-to-r from-indigo-600 to-purple-600">
+        <section className="py-20 bg-indigo-600 ">
           <div className="max-w-4xl mx-auto text-center px-4 sm:px-6 lg:px-8">
             <h2 className="text-4xl font-bold text-white mb-4">
               Ready to Create Your Portfolio?
